46e36649cf378518130eed305684a5eac7553284
Bart Lantz <underhilllabs@gmail.com>
2017-12-11 08:09:54 -0700

    initial commit

---
+++ lib/gh_trends.ex
@@ -0,0 +1,18 @@
+defmodule GhTrends do
+  @moduledoc """
+  Documentation for GhTrends.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GhTrends.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gh_trends_test.exs
@@ -0,0 +1,8 @@
+defmodule GhTrendsTest do
+  use ExUnit.Case
+  doctest GhTrends
+
+  test "greets the world" do
+    assert GhTrends.hello() == :world
+  end
+end
